cfa640e5c6748045d01d610f3b53d2fc3063d094
Grave <graveman13@gmail.com>
2020-02-13 16:45:29 +0300

    add empty line

--- src/main/java/config/AppConfig.java
+++ src/main/java/config/AppConfig.java
@@ -37,9 +37,9 @@ public class AppConfig {
         localSessionFactoryBean.setDataSource(getDataSourse());
 
         Properties properties = new Properties();
-        properties.put("hibernate.show_sql",environment.getProperty("hibernate.show_sql"));
+        properties.put("hibernate.show_sql", environment.getProperty("hibernate.show_sql"));
         properties.put("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
-      //  properties.put("hibernate.dialect", environment.getProperty("hibernate.dialect"));
+        properties.put("hibernate.dialect", environment.getProperty("hibernate.dialect"));
 
         localSessionFactoryBean.setHibernateProperties(properties);
         localSessionFactoryBean.setAnnotatedClasses(User.class);
